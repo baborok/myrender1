53be02d306ef1aa317f93f5df45cdb1a219a4357
baborok <shamil74000@mail.ru>
2023-08-05 13:57:43 +0300

    time

---
+++ src/app — копия.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+
+# Чтение данных из файлов Excel
+df1 = pd.read_excel('06 (1).xls')
+df2 = pd.read_excel('06.xls')
+
+# Создание Dash приложения
+app = dash.Dash(__name__)
+server=app.server
+
+# Макет страницы
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='column-dropdown',
+        options=[{'label': col, 'value': col} for col in df1.columns[1:]],
+        value=df1.columns[1],
+        multi=True
+    ),
+    html.Div(id='graphs-container')
+])
+
+# Обновление графиков при выборе столбца(ов) из выпадающего списка
+@app.callback(
+    Output('graphs-container', 'children'),
+    [Input('column-dropdown', 'value')]
+)
+def update_graphs(selected_columns):
+    graphs = []
+    time_column = 'Время'
+
+    for col in selected_columns:
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df1[time_column], y=df1[col], mode='lines', name=f'Генератор - {col}', line=dict(color='blue')))
+        fig.add_trace(go.Scatter(x=df2[time_column], y=df2[col], mode='lines', name=f'Сеть - {col}', line=dict(color='red')))
+        fig.update_layout(title=f'График {col} по времени',
+                          xaxis_title='Время',
+                          yaxis_title=col)
+
+        graphs.append(
+            dcc.Graph(
+                id=f'graph-{col}',
+                figure=fig
+            )
+        )
+
+    return graphs
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- src/app.py
+++ src/app.py
@@ -1,52 +1,139 @@
+import os
+import base64
+import io
 import pandas as pd
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
-
-# Чтение данных из файлов Excel
-df1 = pd.read_excel('06 (1).xls')
-df2 = pd.read_excel('06.xls')
 
 # Создание Dash приложения
 app = dash.Dash(__name__)
+
 server=app.server
 
 # Макет страницы
 app.layout = html.Div([
     dcc.Dropdown(
+        id='data-folder-dropdown',
+        options=[
+            {'label': '06:00', 'value': '06'},
+            {'label': '07:00', 'value': '07'},
+            {'label': '08:00', 'value': '08'},
+            {'label': '09:00', 'value': '09'},
+            {'label': '10:00', 'value': '10'},
+            {'label': '11:00', 'value': '11'},
+            {'label': '12:00', 'value': '12'},
+            {'label': '13:00', 'value': '13'},
+            {'label': '14:00', 'value': '14'},
+            {'label': '15:00', 'value': '15'},
+            {'label': '16:00', 'value': '16'},
+            {'label': '17:00', 'value': '17'},
+            {'label': '18:00', 'value': '18'}
+        ],
+        value='06',  # Default value is "06"
+        placeholder='Выберите папку с данными',
+        style={'width': '50%'}
+    ),
+    dcc.Dropdown(
         id='column-dropdown',
-        options=[{'label': col, 'value': col} for col in df1.columns[1:]],
-        value=df1.columns[1],
+        options=[],  # Options will be populated based on folder selection
+        value='',   # Default value is an empty string
         multi=True
     ),
     html.Div(id='graphs-container')
 ])
 
-# Обновление графиков при выборе столбца(ов) из выпадающего списка
+def get_file_options(selected_folder):
+    data_folder = os.path.join(os.getcwd(), selected_folder)
+    file_options = []
+    if os.path.exists(data_folder) and os.path.isdir(data_folder):
+        for file in os.listdir(data_folder):
+            if file.endswith('.xls') or file.endswith('.xlsx'):
+                file_options.append({'label': file, 'value': file})
+    return file_options
+
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'xls' in filename:
+            # Excel file format (older version)
+            df = pd.read_excel(io.BytesIO(decoded), engine='xlrd')
+        elif 'xlsx' in filename:
+            # Excel file format (newer version)
+            df = pd.read_excel(io.BytesIO(decoded), engine='openpyxl')
+    except Exception as e:
+        print(e)
+        return None
+
+    return df
+
+@app.callback(
+    Output('column-dropdown', 'options'),
+    Input('data-folder-dropdown', 'value')
+)
+def update_column_dropdown(selected_folder):
+    data_folder = os.path.join(os.getcwd(), selected_folder)
+
+    file1 = None
+    file2 = None
+
+    for selected_file in os.listdir(data_folder):
+        if selected_file.endswith('.xls') or selected_file.endswith('.xlsx'):
+            df = pd.read_excel(os.path.join(data_folder, selected_file), engine='xlrd')
+            if file1 is None:
+                file1 = df
+            elif file2 is None:
+                file2 = df
+
+    if file1 is not None and file2 is not None:
+        combined_df = pd.concat([file1, file2])
+        return [{'label': col, 'value': col} for col in combined_df.columns[1:]]
+    else:
+        return []
+
 @app.callback(
     Output('graphs-container', 'children'),
-    [Input('column-dropdown', 'value')]
+    [Input('data-folder-dropdown', 'value'),
+     Input('column-dropdown', 'value')]
 )
-def update_graphs(selected_columns):
+def update_graphs(selected_folder, selected_columns):
+    data_folder = os.path.join(os.getcwd(), selected_folder)
+
     graphs = []
     time_column = 'Время'
 
-    for col in selected_columns:
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df1[time_column], y=df1[col], mode='lines', name=f'Генератор - {col}', line=dict(color='blue')))
-        fig.add_trace(go.Scatter(x=df2[time_column], y=df2[col], mode='lines', name=f'Сеть - {col}', line=dict(color='red')))
-        fig.update_layout(title=f'График {col} по времени',
-                          xaxis_title='Время',
-                          yaxis_title=col)
+    file1 = None
+    file2 = None
 
-        graphs.append(
-            dcc.Graph(
-                id=f'graph-{col}',
-                figure=fig
-            )
-        )
+    for selected_file in os.listdir(data_folder):
+        if selected_file.endswith('.xls') or selected_file.endswith('.xlsx'):
+            df = pd.read_excel(os.path.join(data_folder, selected_file), engine='xlrd')
+            if file1 is None:
+                file1 = df
+            elif file2 is None:
+                file2 = df
+
+    if file1 is not None and file2 is not None:
+        combined_df = pd.concat([file1, file2])
+
+        for col in selected_columns:
+            if col != time_column:
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(x=file1[time_column], y=file1[col], mode='lines', name=f'Генератор - {col}', line=dict(color='blue')))
+                fig.add_trace(go.Scatter(x=file2[time_column], y=file2[col], mode='lines', name=f'Сеть - {col}', line=dict(color='red')))
+                fig.update_layout(title=f'График {col} по времени',
+                                  xaxis_title='Время',
+                                  yaxis_title=col)
+
+                graphs.append(
+                    dcc.Graph(
+                        id=f'graph-{col}',
+                        figure=fig
+                    )
+                )
 
     return graphs
 
